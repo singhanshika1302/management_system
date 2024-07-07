16336c69310c764ea79559d1f11b34d5577825f5
VIDHIGUPTA17 <vidhigupta1500@gmail.com>
2024-07-07 18:42:46 +0530

    feat:Custom Loader

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:admin_portal/Screens/Login.dart';
-import 'package:admin_portal/Screens/Candidate.dart';
-import 'package:admin_portal/screens/candidate_add.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
