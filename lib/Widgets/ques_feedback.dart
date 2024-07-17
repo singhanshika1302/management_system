2b3ccbe3cb890d9f4cea0add53ef5d98dffaf172
singhanshika1302 <>
2024-07-17 12:56:19 +0530

    Add feedback questions button disabled in case of invalid entrty and toast added

--- lib/Widgets/ques_feedback.dart
+++ lib/Widgets/ques_feedback.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
